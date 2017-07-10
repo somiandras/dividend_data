cf3a63d261e56cdfff4f412efdb15772f8a14187
somiandras <somiandras@gmail.com>
2017-07-10 22:27:52 +0200

    Add check param

--- datahandler/starter.py
+++ datahandler/starter.py
@@ -4,6 +4,6 @@ import subprocess
 
 
 if __name__ == '__main__':
-    env = subprocess.run('source activate dividend && python data_pipeline.py', shell=True, stdout=subprocess.PIPE)
+    env = subprocess.run('source activate dividend && python data_pipeline.py', shell=True, check=True, stdout=subprocess.PIPE)
     if env.returncode == 0:
         print(env.stdout)
